ff97a77b9269dd8db208336412c233257c4bb64e
Yasuhiro Yamada <yamada_ya@tdc.co.jp>
2022-02-02 13:00:12 +0900

    opacity slider smaller

--- src/renderer/SideLayerListItem.tsx
+++ src/renderer/SideLayerListItem.tsx
@@ -84,13 +84,13 @@ export const SideLayerListItem = ({
           alignItems: "center",
           boxSizing: "border-box",
           display: "flex",
-          gap: "0.75rem",
-          padding: "0 1rem 0.5rem 1rem",
+          gap: "0.5rem",
+          padding: "0 1rem 0.25rem 1rem",
           width: "100%",
         }}
       >
         <Box sx={{flexGrow: 1}}>
-          <LayerOpacitySlider id={id} />
+          <LayerOpacitySlider id={id} size='small' />
         </Box>
         <ToggleLayerVisibleButton id={id} />
       </Box>
